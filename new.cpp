c3d92b729d99532676e89540e3efb9720961b1ef
Abhishek Kumar <76006019+allworkdone@users.noreply.github.com>
2023-07-01 19:34:48 +0530

    Before

--- new.cpp
+++ new.cpp
@@ -1,50 +1,146 @@
-void merge(vector<int> arr, int l, int mid, int r)
+#include <limits.h>
+class BST
 {
-    int *arr1 = new int[mid - l + 1];
-    int *arr2 = new int[r - mid];
-    for (int i = 0; i < mid - l + 1; i++)
-        arr1[i] = arr[l + i];
-    for (int j = 0; j < r - mid; j++)
-        arr2[j] = arr[j + mid + 1];
-    int *result = new int[r - l + 1];
-    int i = 0, j = 0, k = 0;
-    while (i < mid - l + 1 && j < r - mid)
+    BinaryTreeNode<int> *root;
+
+    bool searchHelper(int data, BinaryTreeNode<int> *node)
     {
-        if (arr1[i] <= arr2[j])
+        if (node == NULL)
         {
-            result[k] = arr1[i];
-            i++;
-            k++;
+            return false;
+        }
+        if (node->data == data)
+        {
+            return true;
+        }
+        else if (node->data > data)
+        {
+            return searchHelper(data, node->left);
         }
         else
         {
-            result[k] = arr2[j];
-            j++;
-            k++;
+            return searchHelper(data, node->right);
         }
     }
-    while (i < mid - l + 1)
+
+    BinaryTreeNode<int> *insertHelper(int data, BinaryTreeNode<int> *node)
     {
-        result[k] = arr1[i];
-        i++;
-        k++;
+        if (node == NULL)
+        {
+            BinaryTreeNode<int> *n = new BinaryTreeNode<int>(data);
+            return n;
+        }
+        if (root->data >= data)
+        {
+            node->left = insertHelper(data, node->left);
+        }
+        else
+        {
+            node->right = insertHelper(data, node->right);
+        }
+        return node;
     }
-    while (j < r - mid)
+
+    BinaryTreeNode<int> *removeHelper(int data, BinaryTreeNode<int> *node)
     {
-        result[k] = arr2[j];
-        j++;
-        k++;
+        if (node == NULL)
+        {
+            return NULL;
+        }
+        if (data < node->data)
+        {
+            node->left = removeHelper(data, node->left);
+        }
+        else if (data > node->data)
+        {
+            node->right = removeHelper(data, node->right);
+        }
+        else
+        {
+            if (node->left == NULL && node->right == NULL)
+            {
+                delete node;
+                return NULL;
+            }
+            else if (node->right == NULL)
+            {
+                BinaryTreeNode<int> *temp = node->left;
+                delete node;
+                return temp;
+            }
+            else if (node->left == NULL)
+            {
+                BinaryTreeNode<int> *temp = node->right;
+                delete node;
+                return temp;
+            }
+            else
+            {
+                BinaryTreeNode<int> *minnode = node->right;
+                while (minnode->left != NULL)
+                {
+                    minnode = minnode->left;
+                }
+                int replacement = minnode->data;
+                node->data = replacement;
+                node->right = removeHelper(replacement, node->right);
+                return node;
+            }
+        }
     }
-    for (int i = 0; i < r - l + 1; i++)
-        arr[i] = result[i];
-}
-void mergeSort(vector<int> &arr, int l, int r)
-{
-    // Write Your Code Here
-    if (l >= r)
-        return;
-    int mid = (l + r) / 2;
-    mergeSort(arr, l, mid);
-    mergeSort(arr, mid + 1, r);
-    merge(arr, l, mid, r);
-}
+
+    void printHelper(BinaryTreeNode<int> *node)
+    {
+        if (node == NULL)
+            return;
+        cout << node->data << ":";
+        if (node->left)
+        {
+            cout << "L:" << node->left->data << ",";
+        }
+        if (node->right)
+        {
+            cout << "R:" << node->right->data;
+        }
+        cout << endl;
+        if (node->left)
+            printHelper(node->left);
+        if (node->right)
+            printHelper(node->right);
+    }
+
+public:
+    BST()
+    {
+        root = NULL;
+    }
+
+    ~BST()
+    {
+        delete root;
+    }
+
+    void remove(int data)
+    {
+        // Implement the remove() function
+        this->root = removeHelper(data, this->root);
+    }
+
+    void print()
+    {
+        // Implement the print() function
+        printHelper(root);
+    }
+
+    void insert(int data)
+    {
+        // Implement the insert() function
+        this->root = insertHelper(data, this->root);
+    }
+
+    bool search(int data)
+    {
+        // Implement the search() function
+        return searchHelper(data, root);
+    }
+};
