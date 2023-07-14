539f8182d68f0d0d75c6d54c5cd06efebee52d80
Abhishek Kumar <76006019+allworkdone@users.noreply.github.com>
2023-07-14 20:00:58 +0530

    before

--- new.cpp
+++ new.cpp
@@ -1,52 +1,128 @@
-#include <unordered_map>
-vector<int> longestConsecutiveIncreasingSequence(int *arr, int n)
+bool areSame(int arr[], int n)
 {
-    // Your Code goes here
-    unordered_map<int, int> m;
-    for (int i = 0; i < n; i++)
+    int first = arr[0];
+
+    for (int i = 1; i < n; i++)
+        if (arr[i] != first)
+            return false;
+    return true;
+}
+void merge(int *arr, int start, int mid, int end)
+{
+
+    int length1 = mid - start + 1;
+    int length2 = end - mid;
+
+    int *a1 = new int[length1];
+    int *a2 = new int[length2];
+
+    int i = 0;
+    int j = 0;
+    for (int k = start; k <= mid; k++)
     {
-        m[arr[i]]++;
+        a1[i] = arr[k];
+        i++;
     }
-    int start, count, x, maxcount = -1, index1, index2;
-    vector<int> v;
-    for (int i = 0; i < n; i++)
+    for (int k = mid + 1; k <= end; k++)
     {
-        count = 0;
-        start = arr[i];
-        int x = arr[i];
-        while (m.count(x + 1) > 0)
+        a2[j] = arr[k];
+        j++;
+    }
+
+    i = 0;
+    j = 0;
+    int k = start;
+
+    while (i < length1 && j < length2)
+    {
+        if (a1[i] < a2[j])
         {
-            count++;
-            x++;
+            arr[k] = a1[i];
+            i++;
+            k++;
         }
-        while (m.count(start - 1) > 0)
+        else
         {
-            count++;
-            start--;
-        }
-        if (count == 0)
-            v[0] = arr[0];
-        else if (count > maxcount)
-        {
-            v[0] = start;
-            v[1] = start + count;
-            maxcount = count;
-        }
-        else if (count == maxcount)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (arr[i] == v[0])
-                    index1 = i;
-                if (arr[i] == start)
-                    index2 = i;
-            }
-            if (index2 < index1)
-            {
-                v[0] = start;
-                v[1] = start + count;
-            }
+            arr[k] = a2[j];
+            j++;
+            k++;
         }
     }
-    return v;
+    while (i < length1)
+    {
+        arr[k] = a1[i];
+        i++;
+        k++;
+    }
+
+    while (j < length2)
+    {
+        arr[k] = a2[j];
+        j++;
+        k++;
+    }
+
+    delete[] a1;
+    delete[] a2;
+}
+void mergeSort(int *arr, int l, int r)
+{
+
+    if (l >= r)
+    {
+        return;
+    }
+    int mid = (l + r) / 2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+    merge(arr, l, mid, r);
+}
+int pairSum(int *arr, int n, int num)
+{
+    // Write your code here
+    mergeSort(arr, 0, n - 1);
+    int i = 0;
+    int j = n - 1;
+    int count = 0;
+    while (i < j)
+    {
+        if (arr[i] + arr[j] > num)
+        {
+            j--;
+        }
+        else if (arr[i] + arr[j] < num)
+        {
+            i++;
+        }
+        else
+        {
+            areSame(arr, n);
+            if (areSame)
+            {
+                int nig = (n * (n - 1)) / 2;
+                return nig;
+            }
+
+            int k = i;
+            int count1 = 0;
+            while (arr[k] == arr[i])
+            {
+                k++;
+                count1++;
+            }
+            i = k;
+
+            int l = j;
+            int count2 = 0;
+            while (arr[l] == arr[j])
+            {
+                l--;
+                count2++;
+            }
+            j = l;
+
+            count = count + (count1 * count2);
+        }
+    }
+    return count / 2;
 }
